e90efa1df30b63eaa6fc2917400c07c2d50701a7
Aidan Walsh <abwalsh@princeton.edu>
2025-11-18 11:11:02 -0500

    more

--- mapper.py
+++ mapper.py
@@ -4,6 +4,60 @@ import re
 
 
 
+
+
+# first within pivot, we need to enumerate first private network
+# user should be sudo'd into pivot with "sudo su"
+
+def extract_networks():
+  command = ["ip", "a"]
+
+  try:
+      result = subprocess.run(command, stdout=subprocess.PIPE, stderr = subprocess.PIPE) 
+      # extract interfaces and IPs
+      output = "\n" + result.stdout.decode('utf-8')
+      interfaces_info = re.split(r'[\n][0-9]: ',output)[1:]
+  
+      interfaces = []
+      networks = []
+      for info in interfaces_info:
+        interface_rest = info.split(": ")
+
+        inet_rest = interface_rest[1].split("inet ")
+
+        if len(inet_rest) > 1:
+          interfaces.append(interface_rest[0])
+          net_rest = inet_rest[1].split(" ")
+
+          network = net_rest[0]
+          networks.append(network)
+          return networks,interfaces
+  except Exception as e:
+      print(f"An error occurred: {e}")
+      
+    
+      
+          
+def extract_private(networks, interfaces):
+  returned_networks = []
+  returned_interfaces = []
+  for index in range(len(networks)):
+    octets = networks[index].split(".")
+    if int(octets[0]) == 10 or (int(octets[0] == 172) and int(octets[1]) == 16) or (int(octets[0] == 192) and int(octets[1]) == 168):
+      returned_networks.append(networks[index])
+      returned_interfaces.append(interfaces[index])
+      
+    return returned_networks,returned_interfaces
+      
+  
+        
+ 
+        
+        
+      
+
+
+      
 # to be run from the pivot machine 
 credentials_file = "credentials.txt"
 
@@ -21,46 +75,10 @@ with open("credentials.txt", "r") as file_obj:
     except: 
       print("Error parsing credentials file")
       
-
-# first within pivot, we need to enumerate first private network
-# user should be sudo'd into pivot with "sudo su"
-
-sudo_password = "your_sudo_password"  # Replace with your actual password (use with caution!)
-command = ["ip", "a"]
-
-try:
-    result = subprocess.run(command, stdout=subprocess.PIPE, stderr = subprocess.PIPE) 
-    # extract interfaces and IPs
-    
-    output = "\n" + result.stdout.decode('utf-8')
-    #print(output)
-  
-    interfaces_info = re.split(r'[\n][0-9]: ',output)[1:]
-    print(interfaces_info)
-    interfaces = []
-    networks = []
-    for info in interfaces_info:
-      interface_rest = info.split(": ")
-      print(interface_rest)
+all_networks, all_interfaces = extract_networks()
+networks,interfaces = extract_private(all_networks,all_interfaces)
       
       
-      inet_rest = interface_rest[1].split("inet ")
-      print(inet_rest)
-      if len(inet_rest) > 1:
-        interfaces.append(interface_rest[0])
-        net_rest = inet_rest[1].split(" ")
-        print(net_rest)
-        network = net_rest[0]
-        networks.append(network)
-      
-    print(networks)
-    print(interfaces)
-      
-      
-    
-
-except Exception as e:
-    print(f"An error occurred: {e}")
       
       
 
