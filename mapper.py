aea3aad4b629ba18a7256bd2399fb034bbba6f5a
Aidan Walsh <abwalsh@princeton.edu>
2025-11-23 18:52:54 -0500

    more

--- mapper.py
+++ mapper.py
@@ -174,7 +174,7 @@ def scan_network(joined_macs):
           # extract interfaces and IPs
           #output = result.stderr.decode('utf-8')
           
-          print(result.stdout)
+          print(result.stdout.read())
          
     except Exception as e:
           print(f"An error occurred: {e}")   
