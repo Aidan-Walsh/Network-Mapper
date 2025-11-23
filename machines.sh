0b17508bdda79537a2857f41a79ef58c8ee2acc7
Aidan Walsh <abwalsh@princeton.edu>
2025-11-23 18:26:08 -0500

    more

--- machines.sh
+++ machines.sh
@@ -1,7 +1,7 @@
 # configuration to run on all machines which adds sudo user "student"
 # this is only necessary if there is no ssh enabled with passwords and no users
 # are added to the internal networks
-# nano /etc/ssh/sshd_config (change pass auth to yes)
+nano /etc/ssh/sshd_config #(change pass auth to yes)
 sudo adduser student
 sudo usermod -aG sudo student
 sudo systemctl restart sshd
--- pivot.sh
+++ pivot.sh
@@ -3,6 +3,6 @@ nano /etc/ssh/sshd_config #(change pass auth to yes)
 sudo adduser student
 sudo usermod -aG sudo student
 sudo systemctl restart sshd
-Sudo apt update
+sudo apt update
 apt install nmap
 apt install proxychains
