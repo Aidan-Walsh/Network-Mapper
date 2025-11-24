f20bddfb83e34b6206255a5f72f18777cba9f883
Aidan Walsh <abwalsh@princeton.edu>
2025-11-23 19:47:24 -0500

    more

--- scanner.sh
+++ scanner.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in $(seq $2 $3) ;do (ping -c 1 $1.$i | grep "bytes from" &)  1>scan.txt ;done
+for i in $(seq $2 $3) ;do (ping -i 0.1 -c 1 $1.$i | grep "bytes from" &)  2>/dev/null ;done
